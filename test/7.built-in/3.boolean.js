8da6226cd68b2d82aae401811d77c20b4cb030f3
Jeff <jepilworld@gmail.com>
2022-06-19 08:17:33 +1000

    JsPractice_7.built-in_3)boolean

---
+++ test/7.built-in/3.boolean.js
@@ -0,0 +1,27 @@
+// Boolean
+const isTrue = new Boolean(true);
+console.log(isTrue.valueOf());
+
+/**
+ ## Falshy ##
+ * 0
+ * -0
+ * null
+ * NaN
+ * undefined
+ * ''
+ */
+
+/**
+ ## Truthy ##
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+ */
+const isItTrue = 'false';
+if (isItTrue) {
+  console.log('안녕');
+}
