794e326755695162405e0a31db6ae43ce647d486
Jeff <jepilworld@gmail.com>
2022-06-28 22:11:54 +1000

    JsPractice_7.built-in_7)date

---
+++ test/7.built-in/7.date.js
@@ -0,0 +1,25 @@
+//나중에 웹사이트(블로그)를 만들었을때, 게시글을 작성할때마다 박히는 날짜를 구현할때 유용!
+// UTC기준 (협정 세계시, 1970년 1월 1일 UTC 자정과의 시간 차이를 밀리초 단위로 표기)
+// Coordinated Universal Time
+console.log(new Date());
+console.log(new Date('Jun 5, 2022'));
+console.log(new Date('2022-12-17T03:24:00'));
+
+console.log(Date.now());
+console.log(Date.parse('2022-12-17T03:24:00'));
+
+const now = new Date();
+now.setFullYear(2023);
+now.setMonth(0); // 0: 1월, 1: 2월, 2: 3월
+console.log(now.getFullYear());
+console.log(now.getDate()); // 0: 1월, 1: 2월, 2: 3월
+console.log(now.getDay()); // 0 일요일일부터, 1... 6: 토요일
+console.log(now.getTime());
+console.log(now);
+
+console.log(now.toString());
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toISOString()); // ISO 8601 형식
+console.log(now.toLocaleString('en-US'));
+console.log(now.toLocaleString('ko-KR'));
